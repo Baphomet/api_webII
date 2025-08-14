eabac48b2141ea0705ca6d680e2cc773eade1df6
Felipe Grochovski <fgrochovski@gmail.com>
2025-08-14 17:12:10 -0300

    add models

---
+++ Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace webII_API.Models
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+
+    }
+}
