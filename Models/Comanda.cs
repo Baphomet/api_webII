3c3af9f864718891ec291794211b4bfa372d5c29
Felipe Grochovski <fgrochovski@gmail.com>
2025-08-14 17:00:43 -0300

    add models

---
+++ Models/Comanda.cs
@@ -0,0 +1,13 @@
+﻿namespace webII_API.Models
+{
+    public class Comanda
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid IdUsuario { get; set; }
+        public string NomeUsuario { get; set; }
+        public int TelefoneUsuario { get; set; }
+        public ICollection<Produto> Produtos { get; set; } = new List<Produto>();
+        public Usuario Usuario { get; set; }
+
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace webII_API.Models
+{
+    public class Produto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+    }
+}
