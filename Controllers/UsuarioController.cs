595e4380ad25d45408bdc5a03fefa326f908fca5
Felipe Grochovski <fgrochovski@gmail.com>
2025-08-15 17:04:19 -0300

    Adicionado UsuarioController

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webII_API.DTOs;
+using webII_API.Services;
+
+namespace webII_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        public readonly UsuarioService _service;
+
+        public UsuarioController(UsuarioService Usuarioservice)
+        {
+            _service = Usuarioservice;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(await _service.GetAllUsuarios());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] UsuarioDTO dto)
+        {
+            string resultado = await _service.AddUsuario(dto);
+            if (resultado.Contains("Não")) return BadRequest(resultado);
+            return Ok(resultado);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(Guid id, [FromBody] UsuarioDTO dto)
+        {
+            string resultado = await _service.UpdateUsuario(id, dto);
+            if (resultado.Contains("Não encontrado")) return NotFound(resultado);
+            return Ok(resultado);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            string resultado = await _service.DeleteUsuario(id);
+            if (resultado.Contains("Não encontrado")) return NotFound(resultado);
+            return Ok(resultado);
+        }
+    }
+}
--- obj/Debug/net8.0/webII_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/webII_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webII_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617a1a25ea27820e519576650ebf91d6a4223dd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a045f8fe3c383fafc0bcf1e2b3073d4300fba0d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("webII_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webII_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
