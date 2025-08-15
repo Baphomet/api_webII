00065cf1abd0d4379c5bdc318a14d7304227d531
Felipe Grochovski <fgrochovski@gmail.com>
2025-08-15 13:45:01 -0300

    Add DbContext

---
+++ Context/Db.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webII_API.Models;
+
+namespace webII_API.Context
+{
+    public class Db : DbContext
+    {
+        public Db(DbContextOptions<Db> options) : base(options) { }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Comanda> Comandas { get; set; }
+    }
+}
